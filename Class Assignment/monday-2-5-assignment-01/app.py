1c437f13ba731c3b9ea9243cfec236d70f499b3f
laibajaweed <jaweedlaiba0@gmail.com>
2025-03-02 13:12:51 +0500

    comment

---
+++ Class Assignment/monday-2-5-assignment-01/app.py
@@ -0,0 +1,28 @@
+
+print("Hello World!")
+
+# Data Types Example
+
+# Integer
+integer_value = 40
+print(integer_value)
+
+# String
+string_value = "Laiba"
+print(string_value)
+
+# Float
+float_value = 45.12
+print(float_value)
+
+# Boolean
+bool_value = True
+print(bool_value)
+
+# List
+list_value: str = ["Laiba" , "Jaweed",36,False]
+print(list_value)
+
+# Dictionary
+dictionary_value = {"name": "Laiba", "age": 20, "city": "karachi"}
+print(dictionary_value)
